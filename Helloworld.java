c558364ca093d617625c995af2f2135b96441b74
umeshnalkande <umeshnalkande@gmail.com>
2026-01-22 09:05:06 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,6 @@
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("welcome to DevOps day 2 session");
+         System.out.println("welcome to DevOps day 2 session");
     }
 }
